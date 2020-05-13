eba2479f82b861608a00e4cb24f68b033850ba16
Rick Lan <efinilan@gmail.com>
2020-05-13 23:13:26 +1000

    bosch long from gregjhogan

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -11,6 +11,7 @@ import cereal.messaging as messaging
 from selfdrive.config import Conversions as CV
 from selfdrive.boardd.boardd import can_list_to_can_capnp
 from selfdrive.car.car_helpers import get_car, get_startup_alert
+from selfdrive.car.disable_radar import disable_radar
 from selfdrive.controls.lib.lane_planner import CAMERA_OFFSET
 from selfdrive.controls.lib.drive_helpers import get_events, \
                                                  create_event, \
@@ -501,6 +502,8 @@ def controlsd_thread(sm=None, pm=None, can_sock=None):
   params.put("CarParams", cp_bytes)
   put_nonblocking("CarParamsCache", cp_bytes)
   put_nonblocking("LongitudinalControl", "1" if CP.openpilotLongitudinalControl else "0")
+  if CP.openpilotLongitudinalControl and CP.safetyModel in [car.CarParams.SafetyModel.hondaBoschGiraffe, car.CarParams.SafetyModel.hondaBoschHarness]:
+    disable_radar(can_sock, pm.sock['sendcan'], 1 if has_relay else 0, timeout=1, retry=10)
 
   CC = car.CarControl.new_message()
   AM = AlertManager()
@@ -617,7 +620,7 @@ def controlsd_thread(sm=None, pm=None, can_sock=None):
       events.append(create_event('relayMalfunction', [ET.NO_ENTRY, ET.PERMANENT, ET.IMMEDIATE_DISABLE]))
 
     # Only allow engagement with brake pressed when stopped behind another stopped car
-    if not dragon_toyota_stock_dsu and CS.brakePressed and sm['plan'].vTargetFuture >= STARTING_TARGET_SPEED and not CP.radarOffCan and CS.vEgo < 0.3:
+    if not dragon_toyota_stock_dsu and CS.brakePressed and sm['plan'].vTargetFuture >= STARTING_TARGET_SPEED and CP.openpilotLongitudinalControl and CS.vEgo < 0.3:
       events.append(create_event('noTarget', [ET.NO_ENTRY, ET.IMMEDIATE_DISABLE]))
 
     # dp
